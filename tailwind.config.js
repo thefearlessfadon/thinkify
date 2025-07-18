9a07f59a09abd3aef62ca47d8b41c5d2effacbd0
thefearlessfadon <emirtalhaozturk2008@gmail.com>
2025-07-18 17:35:36 +0300

    afa

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1E40AF',
+        secondary: '#F3F4F6',
+      },
+    },
+  },
+  plugins: [],
+}
