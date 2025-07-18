8e0e1de7a408ace46a0d9d99a171bf68f95a5728
thefearlessfadon <emirtalhaozturk2008@gmail.com>
2025-07-18 17:34:16 +0300

    asrfa

--- src/firabase.js
+++ src/firabase.js
@@ -3,12 +3,13 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPasswor
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBAIuCiOUDLY6ZKDBKS0561SlNgiDDvaak",
+  authDomain: "thinkify-2ed59.firebaseapp.com",
+  databaseURL: "https://thinkify-2ed59-default-rtdb.firebaseio.com",
+  projectId: "thinkify-2ed59",
+  storageBucket: "thinkify-2ed59.firebasestorage.app",
+  messagingSenderId: "481098565530",
+  appId: "1:481098565530:web:12d3dfc0ba044c66161c23"
 };
 
 const app = initializeApp(firebaseConfig);
